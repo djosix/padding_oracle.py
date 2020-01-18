b018871852fd9710732c2d8bef1b248200a83fc2
djosix <djosicks@gmail.com>
2020-01-18 14:14:28 +0800

    Version 0.1.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='padding_oracle',
-    version='0.1.2',
+    version='0.1.3',
     author='Yuankui Lee',
     author_email='toregnerate@gmail.com',
     description='Threaded padding oracle automation.',
