0a34ab1ce10ed3420cc547f8fa4017c8c456cf90
djosix <djosicks@gmail.com>
2023-11-18 08:23:14 +0800

    update copyright year

--- src/padding_oracle/solve.py
+++ src/padding_oracle/solve.py
@@ -1,5 +1,5 @@
 '''
-Copyright (c) 2022 Yuankui Li
+Copyright (c) 2023 Yuankui Li
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
